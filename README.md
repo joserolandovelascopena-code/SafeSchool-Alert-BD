15a8aa201e46380f128d9a1b96b39665a2d759a8
joserolandovelascopena-code <joserolandovelascopena@gmail.com>
2026-09-11 19:10:11 -0600

    Integro nuevos cambios en el script de consultas alertas, y finalizo mi aportacion

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 ![Banner repo](./evidenicias/recursos/banner_BD.png)
 
+# Tabla de contenido
+
+- [SafeSchool Alert: Base de Datos](#safeschool-alert-base-de-datos)
+- [Descripción del proyecto](#descripción-del-proyecto)
+- [Base de datos](#base-de-datos)
+  - [Nombre de la base de datos](#nombre-de-la-base-de-datos)
+  - [Sistema gestor](#sistema-gestor)
+- [Estructura de la base de datos](#estructura-de-la-base-de-datos)
+- [Relaciones entre las tablas](#relaciones-entre-las-tablas)
+- [Tecnologías utilizadas](#tecnologías-utilizadas)
+- [Estructura del repositorio](#estructura-del-repositorio)
+- [Integrantes](#integrantes)
+- [Instalación y Configuración](#instalación-y-configuración)
+  - [1. Prerrequisitos](#1-prerrequisitos)
+  - [2. Clonar el repositorio](#2-clonar-el-repositorio)
+
 # SafeSchool Alert: Base de Datos
 
 **Sistema inteligente de alertas y emergencias escolares**
@@ -201,3 +217,5 @@ Luego, clona este repositorio en tu máquina local usando la terminal:
 ```bash
 git clone https://github.com/joserolandovelascopena-code/SafeSchool-Alert-BD.git
 ```
+
+[def]: #
--- database/03_consultas_alertas.sql
+++ database/03_consultas_alertas.sql
@@ -1,12 +1,12 @@
 --ALUMNO: ROLANDO VELASCO
 
 -- 1.2. SELECT: 
--- 1.2.1. CONSULTA 1: Con SELECT, mostrar todos las alertas.
+-- 1.2.1. CONSULTA 1: Mostrar todos las alertas.
 
-  SELECT * FROM public.alertas;
+ SELECT * FROM public.alertas;
 
--- 1.2.2. CONSULTA 2: Con SELECT, mostrar todas las alertas, 
--- con datos compartidos usando LEFT Y INNER JOIN. 
+-- 1.2.2. CONSULTA 2: Realizar un LEFT y un INNER JOIN para obtener datos
+-- más completos de cada emergencia.
 
  SELECT 
     alertas.id_alerta,
@@ -17,14 +17,13 @@
 	alertas.fecha AS dia_detectada,
     alertas.hora_emerg AS hora,
     alertas.estado
-  
  FROM public.alertas
  LEFT JOIN public.usuarios ON alertas.id_usuario = usuarios.id_usuario
  INNER JOIN public.tipos_emergencia ON alertas.id_tipo = tipos_emergencia.id_tipo
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
- WHERE ubicaciones.nombre = 'Edificio A' AND alertas.estado = 'ACTIVA' ;
 
-/* 1.2.3. CONSULTA 3: Con SELECT, mostrar todas las alertas donde el estado sea "ATENDIDA"*/
+
+/* 1.2.3. CONSULTA 3: Con SELECT, mostrar todas las alertas donde el estado sea "ACTIVA"*/
 
  SELECT 
    alertas.id_alerta,
@@ -40,7 +39,7 @@
  LEFT JOIN public.usuarios ON alertas.id_usuario = usuarios.id_usuario
  INNER JOIN public.tipos_emergencia ON alertas.id_tipo = tipos_emergencia.id_tipo
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
- WHERE alertas.estado = 'ATENDIDA';
+ WHERE alertas.estado = 'ACTIVA';
 
 -- 1.3. UPDATE: 
 /* 1.3.1. CONSULTA 1: Cambiar el estado de las emergencias donde estado sea "ACTIVA" */
@@ -48,10 +47,17 @@
  UPDATE public.alertas SET estado = 'ATENDIDA'
  WHERE estado = 'ACTIVA';
 
- /* 1.3.2. CONSULTA 2: Cambiar el usuario de la emergencia, donde el estado sea "EN_PROCESOs" */
+ SELECT * FROM public.alertas
+ WHERE estado = 'Activa';
+ 
+ /* 1.3.2. CONSULTA 2: Cambiar el usuario de la emergencia, donde el estado sea "EN_PROCESO" */
    
  UPDATE public.alertas SET id_usuario = 1
  WHERE estado = 'EN_PROCESO';
+
+
+ SELECT * FROM public.alertas
+ WHERE estado = 'Activa';
 
 -- 1.4. DELETE:
 /* 1.4.1. CONSULTA 1:  Con DELETE */
@@ -80,7 +86,7 @@
 
 -- 2.1. Consulta 1: Mostrar todas las ubicaciones de la intitución educativa.
 
- SELECT * FROM public.ubicaciones;
+ SELECT * FROM public.alertas;
 
 
 -- 2.2. Consulta 2: Filtrar las emergencias utilizando WHERE.
@@ -92,14 +98,14 @@
    usuarios.nombre AS usuario_atendio,
    alertas.descripcion AS descripcion,
    alertas.fecha AS dia_detectada,
-   alertas.hora_emerg AS hora,
    alertas.estado
   
  FROM public.alertas
  LEFT JOIN public.usuarios ON alertas.id_usuario = usuarios.id_usuario
  INNER JOIN public.tipos_emergencia ON alertas.id_tipo = tipos_emergencia.id_tipo
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
- WHERE alertas.id_usuario = 1;
+ WHERE alertas.estado = 'EN_PROCESO';
+
  
 -- 2.3. Consulta 3: Buscar información utilizando otro criterio (WHERE IN).
 
@@ -110,14 +116,13 @@
    usuarios.nombre AS usuario_atendio,
    alertas.descripcion AS descripcion,
    alertas.fecha AS dia_detectada,
-   alertas.hora_emerg AS hora,
    alertas.estado
   
  FROM public.alertas
  LEFT JOIN public.usuarios ON alertas.id_usuario = usuarios.id_usuario
  INNER JOIN public.tipos_emergencia ON alertas.id_tipo = tipos_emergencia.id_tipo
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
- WHERE alertas.estado IN ('ATENDIDA','FALSA');
+ WHERE alertas.estado IN ('ATENDIDA','EN_PROCESO');
 
  -- 2.4. Consulta 4: Ordenar las emergencias por más recientes utilizando ORDER BY.
  
@@ -125,7 +130,6 @@
    alertas.id_alerta,
    tipos_emergencia.tipo AS emergencia,
    ubicaciones.nombre AS ubicacion,
-   usuarios.nombre AS usuario_atendio,
    alertas.descripcion AS descripcion,
    alertas.fecha AS dia_detectada,
    alertas.hora_emerg AS hora,
@@ -137,8 +141,11 @@
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
  ORDER BY alertas.fecha DESC;
 
- 
- -- 2.5. Consulta 5: Mostrar todas las emergencias, que en su discripción tengan la palabra "Humo"
+
+
+ -- 3. CINCO CONSULTAS RELACIONADAS CON EL PROYECTO 
+  
+ -- 3.1. Consulta 1: Mostrar todas las emergencias, que en su discripción tengan la palabra "Humo"
  
  SELECT 
    alertas.id_alerta,
@@ -147,27 +154,21 @@
    usuarios.nombre AS usuario_atendio,
    alertas.descripcion AS descripcion,
    alertas.fecha AS dia_detectada,
-   alertas.hora_emerg AS hora,
    alertas.estado
   
  FROM public.alertas
  LEFT JOIN public.usuarios ON alertas.id_usuario = usuarios.id_usuario
  INNER JOIN public.tipos_emergencia ON alertas.id_tipo = tipos_emergencia.id_tipo
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
- WHERE alertas.descripcion LIKE '%Humo%'
- ORDER BY alertas.fecha DESC;
-
-  
- -- 2.6. Consulta 6:Mostrar todas las emergencias del Edifico A
+ WHERE alertas.descripcion LIKE '%Humo%' OR alertas.descripcion LIKE '%Incendio%';
+ 
+ -- 3.2. Consulta 2: Mostrar todas las emergencias del Edifico A
  
  SELECT 
    alertas.id_alerta,
    tipos_emergencia.tipo AS emergencia,
    ubicaciones.nombre AS ubicacion,
-   usuarios.nombre AS usuario_atendio,
    alertas.descripcion AS descripcion,
-   alertas.fecha AS dia_detectada,
-   alertas.hora_emerg AS hora,
    alertas.estado
   
  FROM public.alertas
@@ -177,7 +178,7 @@
  WHERE ubicaciones.nombre = 'Edificio A';
 
    
- -- 2.7. Consulta 7: Mostrar todas las emergencias de tipo "Incendio"
+ -- 3.3. Consulta 3: Mostrar todas las emergencias de tipo "Incendio"
  
  SELECT 
    alertas.id_alerta,
@@ -195,7 +196,7 @@
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
  WHERE tipos_emergencia.tipo = 'Incendio';
 
- -- 2.8. Consulta 8: Mostrar la cantidad de emergencias registradas por tipo.
+ -- 3.4. Consulta 4: Mostrar la cantidad de emergencias registradas por tipo.
   
  SELECT 
    tipos_emergencia.tipo AS emergencia,
@@ -206,7 +207,7 @@
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
  GROUP BY (tipos_emergencia.tipo);
 
- -- 2.9. Consulta 9: Mostrar la cantidad de emergencias por edificio
+ -- 3.5. Consulta 5: Mostrar la cantidad de emergencias por edificio
   
  SELECT 
    tipos_emergencia.tipo AS emergencia,
@@ -217,16 +218,3 @@
  LEFT JOIN public.tipos_emergencia ON alertas.id_tipo = tipos_emergencia.id_tipo
  INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
  GROUP BY (ubicaciones.nombre, tipos_emergencia.tipo);
-
- -- 2.10. Consulta 10: Mostrar los edificios que tengan almenos dos emergencias
- SELECT 
-    ubicaciones.nombre AS ubicacion,
-    tipos_emergencia.tipo AS emergencia,
-    COUNT(alertas.id_alerta) AS cantidad_emergencias
- FROM public.alertas
- INNER JOIN public.tipos_emergencia ON alertas.id_tipo = tipos_emergencia.id_tipo
- INNER JOIN public.ubicaciones ON alertas.id_ubicacion = ubicaciones.id_ubicacion
- GROUP BY 
-    ubicaciones.nombre,
-    tipos_emergencia.tipo
- HAVING COUNT(alertas.id_alerta) > 1;
